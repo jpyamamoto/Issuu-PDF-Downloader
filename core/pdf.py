66edc707c054467f5a5bf07a7f0cf3fbb193e734
LI Minqiu <matt@LIs-MacBook-Pro.local>
2021-07-22 20:13:49 +0800

    Generate PDF pages based on sizes of images.

--- core/pdf.py
+++ core/pdf.py
@@ -1,11 +1,10 @@
 from fpdf import FPDF
 from PIL import Image
-from framework import pdf_size
 
 pdf = FPDF()
 
 
-def creator(pdflist, pdf_size):
+def creator(pdflist):
     print('Started pdf creation...')
 
     for imageFile in pdflist:
@@ -15,16 +14,9 @@ def creator(pdflist, pdf_size):
         # convert pixel in mm with 1px=0.264583 mm
         width, height = float(width * 0.264583), float(height * 0.264583)
 
-        # get page orientation from image size
-        orientation = 'P' if width < height else 'L'
-
-        #  make sure image size is not greater than the pdf format size
-        width = width if width < pdf_size[orientation]['w'] else pdf_size[orientation]['w']
-        height = height if height < pdf_size[orientation]['h'] else pdf_size[orientation]['h']
-
-        pdf.add_page(orientation=orientation)
+        pdf.add_page(format=(width, height))
 
         pdf.image(imageFile, 0, 0, width, height)
-    pdf.output("file.pdf", "F")
+    pdf.output("output.pdf", "F")
 
     print('Completed pdf creation...')
--- framework.py
+++ framework.py
@@ -24,19 +24,6 @@ def main():
         # If the URL points to the entire document, without any page number
         pass
 
-    # Ask user to set pdf size
-    while True:
-        format_size = input(
-            "Enter 1 if the document is in A4 paper size, or 2 if in B4 paper size:")
-        if format_size == "1":
-            pdf_size = {'P': {'w': 210, 'h': 297}, 'L': {'w': 297, 'h': 210}}
-            break
-        elif format_size == "2":
-            pdf_size = {'P': {'w': 250, 'h': 353}, 'L': {'w': 353, 'h': 250}}
-            break
-        else:
-            print("Please enter 1 or 2 as your input.")
-
     url_open1 = str(urllib.request.urlopen(url).read().decode("utf-8"))
 
     # Credits to https://txt2re.com/ for the regex (Almost all of it)
@@ -50,7 +37,7 @@ def main():
         httpurl = m.group(1)
         print('Starting from URI: ' + httpurl)
         filelist = downloader.downloader(httpurl)
-        pdf.creator(filelist, pdf_size)
+        pdf.creator(filelist)
     else:
         print("Error! No image was found")
 
